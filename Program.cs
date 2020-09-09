7305183050349f2a7f5e8eda530d651c9aedcc13
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-09 20:22:56 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,18 +4,25 @@ namespace toantu
 {
     class Program
     {
-        static void Main(string[] args)
+        float Area(float a, float b)
         {
+            float result = a * b;
+            return result;
+        }
+        static void Main(String[] args)
+        {
+
             float width;
             float height;
-
+            float ketqua;
             Console.Write("Enter width: ");
             width = float.Parse(Console.ReadLine());
             Console.Write("Enter height: ");
             height = float.Parse(Console.ReadLine());
-
-            float area = width * height;
-            Console.WriteLine("Area is: " + area);
+            Program n = new Program();
+            ketqua = n.Area(width, height);
+            Console.WriteLine("Ket qua: {0}", ketqua);
+            Console.ReadLine();
 
         }
     }
